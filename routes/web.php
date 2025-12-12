4aaeb8b9f58db71b08c689cb151946c7b5c05731
jenises5 <mariquitjenises05@gmail.com>
2025-12-12 19:02:53 +0800

    courses

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,33 @@
 <?php
 
+
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\BookmarkController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [CourseController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    
+    // Instructor routes
+    Route::middleware('instructor')->group(function () {
+        Route::get('/courses/create', [CourseController::class, 'create'])->name('courses.create');
+        Route::post('/courses', [CourseController::class, 'store'])->name('courses.store');
+        Route::get('/courses/{course}/edit', [CourseController::class, 'edit'])->name('courses.edit');
+        Route::put('/courses/{course}', [CourseController::class, 'update'])->name('courses.update');
+        Route::delete('/courses/{course}', [CourseController::class, 'destroy'])->name('courses.destroy');
+    });
+
+    // Student routes
+    Route::middleware('student')->group(function () {
+        Route::post('/bookmarks/{course}', [BookmarkController::class, 'toggle'])->name('bookmarks.toggle');
+        Route::post('/bookmarks/{course}/complete', [BookmarkController::class, 'complete'])->name('bookmarks.complete');
+    });
 });
 
-Auth::routes();
+Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
+Route::get('/courses/{course}', [CourseController::class, 'show'])->name('courses.show');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//require __DIR__.'/auth.php';
